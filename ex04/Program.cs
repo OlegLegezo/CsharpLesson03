bde4485c9e2cde715162fbbd59c995fd84c4640e
OlegLegezo <oleglegezo@yandex.ru>
2022-06-03 08:57:39 +0300

    массив отображение и сортировка от минимума

---
+++ ex04/Program.cs
@@ -0,0 +1,45 @@
+﻿// сортировка массива методом min max
+// 1. Найти позицию минимального элемента
+// в неотсортированной части массива
+// 2. Произвести обмен этого значения со
+// значением первой неотсортированной позиции
+// 3. Повторять пока есть не отсортированные
+// элементы
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// метод вывод на экран
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+// метод вывод на экран
+PrintArray(arr);
+
+
+// метод сортировки массива
+void SelectioSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)//не очень понятно зачем тут отнимаем (без этих +- тоже все отрабатывает)
+    {
+        int minPosition = i;
+        //кусок поиска минимального
+        for (int j = i+1; j < array.Length; j++)//не очень понятно зачем тут прибавляем
+        {
+            if (array[j] < array[minPosition])
+                minPosition = j;
+        }
+        //кусок поиска минимального
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+SelectioSort(arr);
+PrintArray(arr);
